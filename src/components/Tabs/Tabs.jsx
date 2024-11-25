7a6f496145e266be3d31b0750d48f76fd82aae33
TargoniyAnatoliy <targoniyanatoliy@gmail.com>
2024-11-25 20:56:05 +0200

    add tabs solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const selectedTabId = tabs.some(tab => tab.id === activeTabId)
+    ? activeTabId
+    : tabs[0].id;
+
+  const activeTab = selectedTabId;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={tab.id === selectedTabId ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                onClick={
+                  tab.id !== selectedTabId
+                    ? () => onTabSelected(tab.id)
+                    : undefined
+                }
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {activeTab && (
+        <div className="block" data-cy="TabContent">
+          {activeTab.content}
+        </div>
+      )}
+    </div>
+  );
+};
